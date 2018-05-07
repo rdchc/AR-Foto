0cf8a8619f4bc4d6d5cad57ea29a04685f9bef1d
Ronald Chan <cch0074@gmail.com>
2018-05-07 23:38:37 +0800

    Add cubes based on planes

--- AR-Foto/ViewController.swift
+++ AR-Foto/ViewController.swift
@@ -10,36 +10,34 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
-        sceneView.delegate = self
-        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        let scene = SCNScene()
-        addCube(in: scene)
+        // Set lightings
+        configLighting(for: sceneView)
         
-        // Set the scene to the view
+        // Create a new scene to the scene view
+        let scene = SCNScene()
         sceneView.scene = scene
-        sceneView.automaticallyUpdatesLighting = true
         
         // Long press gesture recognizer
-//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.didLongPress(with:)))
         sceneView.addGestureRecognizer(longPressRecognizer)
     }
     
     @IBAction func add(_ sender: Any) {
-        addCube(in: sceneView.scene)
+        // Construct cube
+//        let cube = buildCube(in: sceneView.scene)
+        // Add gesture recognizer
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.addObjectToSceneView(with:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
     
     @IBAction func takePhoto(_ sender: Any) {
@@ -52,18 +50,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        setUpSceneView(for: sceneView)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
     
@@ -72,8 +63,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Release any cached data, images, etc that aren't in use.
     }
     
-    // Add a cube in the provided scene
-    func addCube(in scene: SCNScene) {
+// ############################################################################################
+    // MARK: - Self-defined functions
+    
+    // Set up a AR scene view
+    func setUpSceneView(for view: ARSCNView) {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        view.session.run(configuration)
+        
+        view.delegate = self
+        view.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+    
+    // Configure lighting issues
+    func configLighting(for view: ARSCNView) {
+        view.autoenablesDefaultLighting = true
+        view.automaticallyUpdatesLighting = true
+    }
+    
+    // Build a cube
+    func buildCube(in scene: SCNScene) -> SCNNode {
         let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         let material = SCNMaterial()
         material.name = "color"
@@ -82,11 +92,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let boxNode = SCNNode(geometry: box)
         boxNode.name = "myBox"
-        boxNode.position = SCNVector3Make(0, 0, -0.5)
-        scene.rootNode.addChildNode(boxNode)
+        return boxNode
+//        boxNode.position = SCNVector3Make(0, 0, -0.5)
+//        scene.rootNode.addChildNode(boxNode)
     }
     
+    
     // MARK: Gesture recognizers
+    @objc func addObjectToSceneView(with recognizer: UIGestureRecognizer) {
+        // 1. Retrieve hit test results
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        
+        // 2. Get target position
+        guard let hitTestResult = hitTestResults.first else { return }
+        let translation = hitTestResult.worldTransform.translation
+        let x = translation.x
+        let y = translation.y
+        let z = translation.z
+        
+        // 3. Add object to the plane
+        let objNode = buildCube(in: sceneView.scene)
+        objNode.position = SCNVector3(x,y,z)
+        sceneView.scene.rootNode.addChildNode(objNode)
+        
+        // 4. Remove gesture recognizer
+        sceneView.removeGestureRecognizer(recognizer)
+    }
+    
     @objc func didLongPress(with recognizer: UIGestureRecognizer) {
         let tapLocation = recognizer.location(in: sceneView)
         let hitTestResults = sceneView.hitTest(tapLocation)
@@ -95,30 +128,67 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         node.removeFromParentNode()
     }
+}
 
+// ############################################################################################
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    extension ViewController: ARSCNViewDelegate {
+        func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+            // 1. Get plane anchor
+            guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+            
+            // 2. Set up plane
+            let width = CGFloat(planeAnchor.extent.x)
+            let height = CGFloat(planeAnchor.extent.z)
+            let plane = SCNPlane(width: width, height: height)
+            
+            // 3. Set plane color
+            plane.materials.first?.diffuse.contents = UIColor.transparentLightBlue
+            
+            // 4. Bind plane to node
+            let planeNode = SCNNode(geometry: plane)
+            let x = CGFloat(planeAnchor.center.x)
+            let y = CGFloat(planeAnchor.center.y)
+            let z = CGFloat(planeAnchor.center.z)
+            planeNode.position = SCNVector3(x, y, z)
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            // 5. Add plane node
+            print("Adding plane")
+            node.addChildNode(planeNode)
+        }
         
+        func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+            // 1. Get plane anchors and plane
+            guard let planeAnchor = anchor as? ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+                else { return }
+            
+            // 2. Update plane width and depth
+            let width = CGFloat(planeAnchor.extent.x)
+            let depth = CGFloat(planeAnchor.extent.z)
+            plane.width = width
+            plane.height = depth
+            
+            // 3. Update plane node position
+            let x = CGFloat(planeAnchor.center.x)
+            let y = CGFloat(planeAnchor.center.y)
+            let z = CGFloat(planeAnchor.center.z)
+            planeNode.position = SCNVector3(x, y, z)
+        }
     }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+
+// MARK: - Extensions
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
     }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+}
+
+extension UIColor {
+    open class var transparentLightBlue: UIColor {
+        return UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 0.50)
     }
 }
